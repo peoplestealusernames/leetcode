bb9e219c11f6229252b3758042288ca8d16befc9
peoplestealusernames <50470278+peoplestealusernames@users.noreply.github.com>
2022-05-12 13:10:20 -0600

    3. Revamp using a found string and math to loop once

--- 3.longest-substring-without-repeating-characters.ts
+++ 3.longest-substring-without-repeating-characters.ts
@@ -7,32 +7,32 @@
 // @lc code=start
 function lengthOfLongestSubstring(s: string): number {
     let largest = 0;
+    let found = ""
+
+    // 0 1 2 3 4 5 6 7
+    // a b c a b c b b
 
     for (let i = 0; i < s.length; i++) {
-        const run = untilRepeat(s.substring(i, s.length))
-        if (run > largest) largest = run
+        const at = found.indexOf(s[i])
+        if (at != -1) {
+            const leng = found.length
+            if (leng > largest) largest = leng
+            found = found.slice(at + 1)
+        }
+        found += s[i]
     }
+
+    const leng = found.length
+    if (leng > largest) largest = leng
 
     return largest
 };
 
-function untilRepeat(s: string): number {
-    let found = ""
-    let i = 0
-
-    for (; i < s.length; i++)
-        if (found.includes(s[i]))
-            return i
-        else
-            found += s[i]
-
-    return i
-}
 // @lc code=end
 
 /* working: 5/12/2022
  * Accepted
- ** 987/987 cases passed (534 ms)
- ** Your runtime beats 15.35 % of typescript submissions
- ** Your memory usage beats 18.65 % of typescript submissions (50.6 MB)
+ ** 987/987 cases passed (96 ms)
+ ** Your runtime beats 87.42 % of typescript submissions
+ ** Your memory usage beats 57.67 % of typescript submissions (48.7 MB)
  */
